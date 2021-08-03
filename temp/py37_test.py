de08a35396ccdefe51bb81fb0b9d8f076d255d44
kuisu <kuisu_dgut@163.com>
2021-08-03 10:42:17 +0800

    Initial commit

---
+++ temp/py37_test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+'''
+@Project ：Project_subject 
+@File    ：py37_test.py
+@Author  ：kuisu
+@Email     ：kuisu_dgut@163.com
+@Date    ：2021/8/3 9:39 
+'''
+print('hello, world')
